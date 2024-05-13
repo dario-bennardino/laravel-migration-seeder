bc383e2721a2d31be44eadafab6a60e40a609b68
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-05-13 16:48:30 +0200

    [add] esercizio completo

--- database/migrations/2024_05_13_122002_create_trains_table.php
+++ database/migrations/2024_05_13_122002_create_trains_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Laravel\SerializableClosure\UnsignedSerializableClosure;
 
 return new class extends Migration
 {
@@ -13,6 +14,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 40);
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->mediumInteger('train_code')->unsigned();
+            $table->tinyInteger('carriage_number');
+            $table->boolean('is_on_time')->default(true);
+            $table->tinyInteger('delay')->nullable();
             $table->timestamps();
         });
     }
